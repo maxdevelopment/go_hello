2e7561c78f7f9d16d1d9c7884edbccdb4fa61d81
Max.N <gtamadmax@gmail.com>
2017-11-14 00:43:52 +0300

    print system environment

--- hello.go
+++ hello.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+	fmt.Println("System environment:")
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Printf("Name: %s | Value: %s\n", pair[0], pair[1])
+	}
 }
