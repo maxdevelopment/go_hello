95c65fa22ad7caba26ea9cdce19bdd488a226b56
Max Naranovich <gtamadmax@gmail.com>
2017-11-16 02:28:22 +0300

    go-colortext

--- hello.go
+++ hello.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"github.com/daviddengcn/go-colortext"
 )
 
 func main() {
 	fmt.Println("System environment:")
 	for _, e := range os.Environ() {
 		pair := strings.Split(e, "=")
-		fmt.Printf("Name: %s | Value: %s\n", pair[0], pair[1])
+		fmt.Printf("Name: %s", pair[0])
+		ct.Foreground(ct.Yellow, false)
+		fmt.Printf(" Value: %s\n", pair[1])
+		ct.ResetColor()
 	}
 }
