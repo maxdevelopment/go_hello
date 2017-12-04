8a46dbf58de52d524b806a9fde72fe474a7d3f42
Max Naranovich <gtamadmax@gmail.com>
2017-12-05 02:48:13 +0300

    types

--- hello.go
+++ hello.go
@@ -2,18 +2,44 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"github.com/daviddengcn/go-colortext"
+	"strconv"
+	"reflect"
+	//"os"
+	//"strings"
+	//"github.com/daviddengcn/go-colortext"
 )
 
 func main() {
-	fmt.Println("System environment:")
-	for _, e := range os.Environ() {
-		pair := strings.Split(e, "=")
-		fmt.Printf("Name: %s", pair[0])
-		ct.Foreground(ct.Yellow, false)
-		fmt.Printf(" Value: %s\n", pair[1])
-		ct.ResetColor()
+	stringValue := "24.90"
+	stringToBool, err := strconv.ParseBool(stringValue)
+	stringToFloat, err := strconv.ParseFloat(stringValue, 64)
+
+	intNumber := 34
+
+	intNumberToStr := strconv.Itoa(intNumber)
+
+	i, err := strconv.ParseFloat(intNumberToStr, 32)
+	intNumberToFloat32 := float32(i)
+	intNumberToFloat64 := float64(i)
+	if err != nil {
+		panic(err)
 	}
+
+	fmt.Printf("%v with type %s\n", stringToBool, reflect.TypeOf(stringToBool))
+	fmt.Printf("%v with type %s\n", stringToFloat, reflect.TypeOf(stringToFloat))
+	fmt.Printf("%v with type %s\n", intNumber, reflect.TypeOf(intNumber))
+	fmt.Printf("%v with type %s\n", intNumberToStr, reflect.TypeOf(intNumberToStr))
+	fmt.Printf("%v with type %s\n", intNumberToFloat32, reflect.TypeOf(intNumberToFloat32))
+	fmt.Printf("%v with type %s\n", intNumberToFloat64, reflect.TypeOf(intNumberToFloat64))
+
+
+
+	//fmt.Println("System environment:")
+	//for _, e := range os.Environ() {
+	//	pair := strings.Split(e, "=")
+	//	fmt.Printf("Name: %s", pair[0])
+	//	ct.Foreground(ct.Yellow, false)
+	//	fmt.Printf(" Value: %s\n", pair[1])
+	//	ct.ResetColor()
+	//}
 }
