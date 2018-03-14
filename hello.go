d06422e22164467a0abf051fe549428e046ecb59
Max Naranovich <gtamadmax@gmail.com>
2018-03-15 02:28:29 +0300

    goroutines and map

--- hello.go
+++ hello.go
@@ -3,33 +3,51 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
+const maxMapSize = 10
+
+func random(min int, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func logger(channel <-chan int) {
+	data := make(map[int]int)
+	for {
+		msg := <-channel
+
+		if len(data) > maxMapSize {
+			for i := range data {
+				delete(data, i)
+			}
+		}
+		data[msg] = msg
+		fmt.Println(data)
+	}
+}
+
+func randData(channel chan<- int) {
+	for {
+		channel <- random(0, 10)
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func randDataExt(channel chan<- int) {
+	for {
+		channel <- random(100, 200)
+		time.Sleep(time.Second * 3)
+	}
+}
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	channel := make(chan int)
+	go logger(channel)
+	go randData(channel)
+	go randDataExt(channel)
 
-	array := [10]int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
-	fmt.Printf("created array: %v \narray length: %v\n\n", array, len(array))
-
-	slice := array[1:5]
-	fmt.Printf("created slice: %v \nslice length: %v\nslice capacity: %v\n\n", slice, len(slice), cap(slice))
-
-	slice[1] = 33
-	fmt.Printf("changed slice element: %v \nslice length: %v\nslice capacity: %v\n\n", slice, len(slice), cap(slice))
-
-	slice = append(slice, 55)
-	fmt.Printf("added slice element: %v \nslice length: %v\nslice capacity: %v\n\n", slice, len(slice), cap(slice))
-
-	arrayMod := [10]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
-	fmt.Printf("array for modifications: %v\n", arrayMod)
-
-	for i, j := 0, len(arrayMod)-1; i < j; i, j = i+1, j-1 {
-		arrayMod[i], arrayMod[j] = arrayMod[j], arrayMod[i]
-	}
-	fmt.Printf("reversed: %v\n", arrayMod)
-
-	for i := range arrayMod {
-		j := rand.Intn(i + 1)
-		arrayMod[i], arrayMod[j] = arrayMod[j], arrayMod[i]
-	}
-	fmt.Printf("shuffled: %v\n", arrayMod)
+	var input string
+	fmt.Scanln(&input)
 }
