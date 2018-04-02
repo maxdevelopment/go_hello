14c33b770fc6d01976fbbfe10f4b4312dfcd80f2
Max Naranovich <gtamadmax@gmail.com>
2018-04-02 19:42:33 +0300

    jff

--- hello.go
+++ hello.go
@@ -2,34 +2,34 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
+type example struct {
+	flag    bool
+	counter int32
+	pi      float64
+}
+
+type MyInt int
+
+func (m MyInt) showYourSelf() {
+	fmt.Printf("%T %v\n", m, m)
+}
+
+func (m *MyInt) add(i MyInt) {
+	*m = *m + MyInt(i)
+}
+
 func main() {
-
-	array := [10]int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
-	fmt.Printf("created array: %v \narray length: %v\n\n", array, len(array))
-
-	slice := array[1:5]
-	fmt.Printf("created slice: %v \nslice length: %v\nslice capacity: %v\n\n", slice, len(slice), cap(slice))
-
-	slice[1] = 33
-	fmt.Printf("changed slice element: %v \nslice length: %v\nslice capacity: %v\n\n", slice, len(slice), cap(slice))
-
-	slice = append(slice, 55)
-	fmt.Printf("added slice element: %v \nslice length: %v\nslice capacity: %v\n\n", slice, len(slice), cap(slice))
-
-	arrayMod := [10]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
-	fmt.Printf("array for modifications: %v\n", arrayMod)
-
-	for i, j := 0, len(arrayMod)-1; i < j; i, j = i+1, j-1 {
-		arrayMod[i], arrayMod[j] = arrayMod[j], arrayMod[i]
+	ex1 := example{}
+	ex2 := example{
+		flag: true,
 	}
-	fmt.Printf("reversed: %v\n", arrayMod)
+	mi := MyInt(100)
+	mi.showYourSelf()
+	mi.add(50)
+	mi.showYourSelf()
+	fmt.Printf("%+v", ex1)
+	fmt.Printf("%+v", ex2)
 
-	for i := range arrayMod {
-		j := rand.Intn(i + 1)
-		arrayMod[i], arrayMod[j] = arrayMod[j], arrayMod[i]
-	}
-	fmt.Printf("shuffled: %v\n", arrayMod)
 }
